fed522a1e50d4f536d9248b762174534d5f33da1
Mariangelica Useche <musenberg404@gmail.com>
2019-02-21 15:56:52 -0400

    Added some colors

--- recipesmus/theme/colors.js
+++ recipesmus/theme/colors.js
@@ -1,9 +1,17 @@
 const Colors = {
+    mainBackground: "#f7f7f7",
     transparent: "rgba(0,0,0,0)",
-    whiteFull: "#eeeeee",
-    blackFull: "333333",
-    redFull: "#fff000",
-    darkOrange: "#de1738"
+    blackFull: "#333333",
+    redFull: "#ff0000",
+    lightOrange: "#ff8c2b",
+    darkOrange: "#ff6322",
+    black: "#333333",
+    darkGrey: "#9e9e9e",
+    mediumGrey: "#bdbdbd",
+    gray: "#e0e0e0",
+    lightGrey: "#f4f6f6",
+    darkWhite: "#f7f7f7",
+    whiteFull: "#ffffff"
 }
 
 export default Colors;
